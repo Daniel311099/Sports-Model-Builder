dd91a75a306cc4779dc78a87589389c987668133
Daniel Fisaha <danielfisaha@gmail.com>
2023-08-13 00:30:34 +0100

    task manager boilerplate

--- src/backend/etl/task_manager/app.py
+++ src/backend/etl/task_manager/app.py
@@ -3,9 +3,14 @@ from manager import Manager
 # parse command line arguments
 # if cli flag then run cli
 def main():
-    manager = Manager()
     # main2 script
-    job = input('Enter job: ')
+    manager = Manager()
+    manager.run()
+
+    # if flag render cli
+    app = App(manager)
+    # app fetchtes all data from manager
+    app.render()
     # on new job
         # pause schedule
         # add job to ledger
@@ -14,3 +19,11 @@ def main():
         # update schedule
         # resume schedule
             # add tasks to queue
+
+
+class App:
+    def __init__(self, manager: Manager)):
+        self.manager = manager
+
+    def render(self):
+        ...
